30e0cdc09b8f7061f89f3f3ca4b78995fc9bff91
kimdohyung <pdohyung@gmail.com>
2023-12-15 14:54:45 +0900

    feat(MenuController): 월요일부터 무작위 카테고리를 선정해서 각 코치가 먹을 메뉴를 추천하고 결과를 출력한다.

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,6 +1,8 @@
 package menu.controller;
 
 import menu.domain.Coach;
+import menu.domain.enums.Day;
+import menu.repository.MenuRecommendRepository;
 import menu.view.InputView;
 import menu.view.OutputView;
 
@@ -10,10 +12,12 @@ import java.util.List;
 public class MenuController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final MenuRecommendRepository menuRecommendRepository;
 
     public MenuController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.menuRecommendRepository = new MenuRecommendRepository();
     }
 
     public void run() {
@@ -23,6 +27,17 @@ public class MenuController {
         for (String coachName : coachNames) {
             List<String> canNotEatMenus = inputView.inputCanNotEat(coachName);
             coaches.add(new Coach(coachName, canNotEatMenus));
+        }
+        recommend(coaches);
+        outputView.printMenuRecommendResult(menuRecommendRepository.getMenuRecommendResult());
+        outputView.printMenuRecommendEnd();
+    }
+
+    public void recommend(List<Coach> coaches) {
+        for (Day day : Day.values()) {
+            for (Coach coach : coaches) {
+                menuRecommendRepository.createRecommend(day, coach);
+            }
         }
     }
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,10 +1,20 @@
 package menu.view;
 
+import menu.domain.Coach;
+import menu.domain.MenuRecommend;
+import menu.domain.enums.Day;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import static java.text.MessageFormat.*;
 
 public class OutputView {
     private static final String PRINT_MENU_RECOMMEND_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     private static final String PRINT_MENU_RECOMMEND_END_MESSAGE = "\n추천을 완료했습니다.";
+    private static final String PRINT_MENU_RECOMMEND_RESULT_START_MESSAGE = "메뉴 추천 결과입니다.";
     private static final String ERROR_MESSAGE_FORMAT = "\n[ERROR] {0}";
 
     public void printErrorMessage(String errorMessage) {
@@ -17,5 +27,62 @@ public class OutputView {
 
     public void printMenuRecommendEnd() {
         System.out.println(PRINT_MENU_RECOMMEND_END_MESSAGE);
+    }
+
+    public void printMenuRecommendResult(Map<Coach, List<MenuRecommend>> menuRecommendResult) {
+        Day[] days = Day.values();
+        System.out.println(PRINT_MENU_RECOMMEND_RESULT_START_MESSAGE);
+        printHeader(days);
+        printCategories(menuRecommendResult, days);
+        printRecommendations(menuRecommendResult, days);
+    }
+
+    private void printHeader(Day[] days) {
+        System.out.println("[ 구분 | " + Arrays.stream(days)
+                .map(Day::getName)
+                .collect(Collectors.joining(" | ")) + " ]");
+    }
+
+    private void printCategories(Map<Coach, List<MenuRecommend>> menuRecommendResult, Day[] days) {
+        System.out.print("[ 카테고리 | ");
+        for (int i = 0; i < days.length; i++) {
+            printCategoryForDay(menuRecommendResult, days[i]);
+            if (i < days.length - 1) {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println(" ]");
+    }
+
+    private void printCategoryForDay(Map<Coach, List<MenuRecommend>> menuRecommendResult, Day day) {
+        String category = menuRecommendResult.values().stream()
+                .flatMap(List::stream)
+                .filter(mr -> mr.getDay().equals(day))
+                .findFirst()
+                .map(mr -> mr.getCategory().getName())
+                .orElse("NONE");
+        System.out.print(category);
+    }
+
+    private void printRecommendations(Map<Coach, List<MenuRecommend>> menuRecommendResult, Day[] days) {
+        for (Map.Entry<Coach, List<MenuRecommend>> entry : menuRecommendResult.entrySet()) {
+            System.out.print("[ " + entry.getKey().getName() + " | ");
+            for (int i = 0; i < days.length; i++) {
+                printDishForDay(entry.getValue(), days[i]);
+                if (i < days.length - 1) {
+                    System.out.print(" | ");
+                }
+            }
+            System.out.println(" ]");
+        }
+    }
+
+    private void printDishForDay(List<MenuRecommend> menuRecommends, Day day) {
+        String dish = menuRecommends.stream()
+                .filter(mr -> mr.getDay().equals(day))
+                .findFirst()
+                .map(MenuRecommend::getDish)
+                .orElse("NONE");
+        System.out.print(dish);
     }
 }
