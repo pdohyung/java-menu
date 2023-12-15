21ba8f28adc9876ecc9f461051a59139361e5acb
kimdohyung <pdohyung@gmail.com>
2023-12-15 10:58:30 +0900

    feat(OutputView): 메뉴 추천 실행시 `점심 메뉴 추천을 시작합니다.`를 출력한다.

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,9 @@
+package menu.view;
+
+public class OutputView {
+    private static final String PRINT_MENU_RECOMMEND_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+
+    public void printMenuRecommendStart() {
+        System.out.println(PRINT_MENU_RECOMMEND_START_MESSAGE);
+    }
+}
