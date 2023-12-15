e90df6008150c3a9b61a2adf44fcc3b84ea5f9d7
kimdohyung <pdohyung@gmail.com>
2023-12-15 11:02:08 +0900

    feat(OutputView): 예외 발생 시 `[ERROR]`로 시작하는 에러 메시지를 출력한다.

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,8 +1,15 @@
 package menu.view;
 
+import static java.text.MessageFormat.*;
+
 public class OutputView {
     private static final String PRINT_MENU_RECOMMEND_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     private static final String PRINT_MENU_RECOMMEND_END_MESSAGE = "\n추천을 완료했습니다.";
+    private static final String ERROR_MESSAGE_FORMAT = "\n[ERROR] {0}";
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(format(ERROR_MESSAGE_FORMAT, errorMessage));
+    }
 
     public void printMenuRecommendStart() {
         System.out.println(PRINT_MENU_RECOMMEND_START_MESSAGE);
