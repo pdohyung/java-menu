9b4f1a0e64ee8058cf1f2a90799df79cb06bc8a8
kimdohyung <pdohyung@gmail.com>
2023-12-15 12:48:38 +0900

    feat(MenuController): 코치마다 못 먹는 음식을 입력받는다.

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,8 +1,10 @@
 package menu.controller;
 
+import menu.domain.Coach;
 import menu.view.InputView;
 import menu.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuController {
@@ -17,5 +19,10 @@ public class MenuController {
     public void run() {
         outputView.printMenuRecommendStart();
         List<String> coachNames = inputView.inputCoachNames();
+        List<Coach> coaches = new ArrayList<>();
+        for (String coachName : coachNames) {
+            List<String> canNotEatMenus = inputView.inputCanNotEat(coachName);
+            coaches.add(new Coach(coachName, canNotEatMenus));
+        }
     }
 }
---
+++ src/main/java/menu/domain/Coach.java
@@ -0,0 +1,21 @@
+package menu.domain;
+
+import java.util.List;
+
+public class Coach {
+    private final String name;
+    private final List<String> canNotEatMenus;
+
+    public Coach(String name, List<String> canNotEatMenus) {
+        this.name = name;
+        this.canNotEatMenus = canNotEatMenus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getCanNotEatMenus() {
+        return canNotEatMenus;
+    }
+}
