4d416b91398d7d24e01a21f6d3810531a75b3ec7
kimdohyung <pdohyung@gmail.com>
2023-12-15 11:00:14 +0900

    feat(OutputView): 메뉴 추천 종료 시 `추천을 완료했습니다.`를 출력한다.

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,8 +2,13 @@ package menu.view;
 
 public class OutputView {
     private static final String PRINT_MENU_RECOMMEND_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private static final String PRINT_MENU_RECOMMEND_END_MESSAGE = "\n추천을 완료했습니다.";
 
     public void printMenuRecommendStart() {
         System.out.println(PRINT_MENU_RECOMMEND_START_MESSAGE);
+    }
+
+    public void printMenuRecommendEnd() {
+        System.out.println(PRINT_MENU_RECOMMEND_END_MESSAGE);
     }
 }
